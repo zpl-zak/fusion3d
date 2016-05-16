50956b1840181aff60826eda01b13ab8eb5ea8a1
Dominik Madarász <zaklaus@subvision-studio.com>
2016-05-16 20:32:16 +0200

    Updated license.

--- engine/src/components/GUIWindow.h
+++ engine/src/components/GUIWindow.h
@@ -1,6 +1,6 @@
 #pragma once
 /*
-* Copyright (C) 2015 Subvision Studio
+* Copyright (C) 2015-2016 Subvision Studio
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
--- engine/src/components/rigidBody.cpp
+++ engine/src/components/rigidBody.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Subvision Studio
+ * Copyright (C) 2015-2016 Subvision Studio
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@
  * @author Subvision Studio 
  * @section LICENSE
  *
- * Copyright (C) 2015 Subvision Studio
+ * Copyright (C) 2015-2016 Subvision Studio
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
