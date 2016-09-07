1233a43e96242930cfbbc93bcec4fbb752fec5cd
Dominik Madarász <madaraszd@mail.ru>
2016-09-07 20:57:45 +0200

    Copyright change, to make it valid.

--- engine/src/core/coreEngine.h
+++ engine/src/core/coreEngine.h
@@ -1,5 +1,6 @@
-/**
- * Copyright (C) 2015-2016 Subvision Studio
+/*
+ * Copyright (C) 2015-2016 Dominik "ZaKlaus" Madarasz
+ * Copyright (C) 2014 Benny Bobaganoosh
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #ifndef COREENGINE_H
 #define COREENGINE_H
 
