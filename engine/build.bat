4dba80b0b008e202fd101eed9be2d47ec226c7b2
Dominik Madarász <madaraszd@mail.ru>
2017-01-06 16:03:44 +0100

    Huge changes (literally huge.)

--- engine/build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
-devenv "%1" /Build "%2"
-@echo on
--- engine/data/tasks/ping.py
+++
@@ -1,2 +0,0 @@
-import sys
-print "Pong to " + sys.argv[1]
